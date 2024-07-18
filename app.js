8dab2cf53e738b0dea48541bcd567e312f145bbe
baburali <baburali1910@gmail.com>
2024-07-18 01:38:05 -0700

    add file

---
+++ app.js
@@ -0,0 +1,132 @@
+// console.log("hello");
+
+
+// arithmatic operators
+
+// task-1===============
+
+// let a = 20;
+// let b = 10;
+// console.log("a+b = " , a+b);
+
+// // task-2================
+
+// let a = 20;
+// let b = 10;
+// console.log("a-b = " , a-b);
+
+// // task-3================
+
+// let a = 20;
+// let b = 10;
+// console.log("a*b = " , a*b);
+
+// task-4==================
+
+// let a = 20;
+// let b = 10;
+// console.log("a/b = " , a/b);
+
+// task-2
+
+// let a = 21;
+// let b = 10;
+// console.log("a%b = " , a%b);
+
+
+// assignment operators==============
+
+// task-5==================
+
+// let num1 = 20;
+
+// num1 += 12;
+
+// console.log("1st" , num1);   or
+
+// num1 = num1 + 12;
+
+// console.log("2nd", num1);
+
+
+// task-6==================
+
+// let num1 = 23;
+
+// num1 -= 3;
+// console.log(num1);
+
+// num1 = num1 - 3;
+// console.log(num1);
+
+
+// comparison operators==============
+
+// task-7==================
+
+// let a = 5;
+
+// let b = 6;
+
+// console.log("a > b", a > b);
+// console.log("a < b", a < b);
+
+
+// task-8==================
+
+
+// let num1 = 12;
+
+// let num2 = 11;
+
+// console.log("num1 >= num2", num1 >= num2);
+// console.log("num1 =< num2", num1 <= num2);
+
+
+// task-9==================
+
+// let num1 = 20;
+
+// let str1 = "20";
+// == donnot bcheck the data type
+// console.log("num1 == str1", num1 == str1);
+
+// === check data type
+// console.log("num1 === str1", num1 === str1);
+
+
+// logical operators==============
+
+// let x = 5;
+
+// let y = 6;
+
+// console.log("x > y && x === y", x > y && x == y);
+// console.log("x < y && x === y", x < y || x == y);
+// console.log("x < y && x != y", x < y || x != y);
+
+// ternary operators==============
+
+// let x = 5;
+
+// let y = 6;
+
+// x < y ? console.log("sai hai ") : console.log("kuch nahi ho skta hai");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
